8625631406e31ccf371c72452d71264d9f953029
MMcClure11 <mcclme02@luther.edu>
2021-02-26 16:25:07 -0600

    Add person to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
+       <Person />
       </div>
     );
     //The above is equivalent to the below, it is what React does for us, JSX is much easier to understand
