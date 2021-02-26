a0fea13b79f23658c5318569b9dd9451185eac0f
MMcClure11 <mcclme02@luther.edu>
2021-02-26 16:55:58 -0600

    Add switchNameHandler

--- src/App.js
+++ src/App.js
@@ -12,14 +12,25 @@ class App extends Component {
     ] 
   }
 
+  switchNameHandler = () => {
+    // console.log('Was clicked!')
+    this.setState({ 
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ] 
+     });
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
-       <button>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+       <button onClick={ this.switchNameHandler }>Switch Name</button>
+       <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
+       <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>My hobbies: Racing</Person>
+       <Person name={ this.state.persons[2].name }  age={ this.state.persons[2].age } />
       </div>
     );
     //The above is equivalent to the below, it is what React does for us, JSX is much easier to understand
