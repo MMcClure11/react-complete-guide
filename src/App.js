b6ab1a97cbd5e81006c2f3a10ab358dd498fe599
MMcClure11 <mcclme02@luther.edu>
2021-02-26 16:47:09 -0600

    Add state to App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ] 
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
-       <Person name="Max" age="28"/>
-       <Person name="Manu" age="29">My hobbies: Racing</Person>
-       <Person name="Stephanie" age="31" />
+       <button>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     //The above is equivalent to the below, it is what React does for us, JSX is much easier to understand
