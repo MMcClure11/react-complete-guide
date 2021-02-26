e4ee2aaff6da05d7f758b597933c1c75eeb6dabc
MMcClure11 <mcclme02@luther.edu>
2021-02-26 17:08:14 -0600

    Refactor App to use hooks for state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const app = () => {
 
-  state = {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ] 
-  }
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // console.log('Was clicked!')
-    this.setState({ 
+    setPersonsState({ 
       persons: [
         { name: 'Maximilian', age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ] 
+      //note: hooks do not add to existing state, it overwrites it
      });
-  }
-
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi, I'm a React App</h1>
-       <button onClick={ this.switchNameHandler }>Switch Name</button>
-       <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
-       <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>My hobbies: Racing</Person>
-       <Person name={ this.state.persons[2].name }  age={ this.state.persons[2].age } />
-      </div>
-    );
-    //The above is equivalent to the below, it is what React does for us, JSX is much easier to understand
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
-  }
+  };
+ 
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <button onClick={ switchNameHandler }>Switch Name</button>
+      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }/>
+      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }>My hobbies: Racing</Person>
+      <Person name={ personsState.persons[2].name }  age={ personsState.persons[2].age } />
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
 }
 
-export default App;
+export default app;
+
+// state = {
+//   persons: [
+//     { name: 'Max', age: 28 },
+//     { name: 'Manu', age: 29 },
+//     { name: 'Stephanie', age: 26 }
+//   ] 
+// }
+
+// switchNameHandler = () => {
+//   // console.log('Was clicked!')
+//   this.setState({ 
+//     persons: [
+//       { name: 'Maximilian', age: 28 },
+//       { name: 'Manu', age: 29 },
+//       { name: 'Stephanie', age: 27 }
+//     ] 
+//    });
+// }
