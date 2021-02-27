d97f439b8672d4d9ec9a2e809fb371a1eafaffe1
MMcClure11 <mcclme02@luther.edu>
2021-02-27 17:46:55 -0600

    Add notes on passing functions as props to children

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ const app = () => {
     ] 
   });
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     // console.log('Was clicked!')
     setPersonsState({ 
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ] 
@@ -27,10 +27,19 @@ const app = () => {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
-      <button onClick={ switchNameHandler }>Switch Name</button>
-      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }/>
-      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }>My hobbies: Racing</Person>
-      <Person name={ personsState.persons[2].name }  age={ personsState.persons[2].age } />
+      <button onClick={ () => switchNameHandler('Maximilian!!') }>Switch Name</button>
+      {/* the above anonymous arrow function can be inefficient */}
+      <Person 
+        name={ personsState.persons[0].name } 
+        age={ personsState.persons[0].age }/>
+      <Person 
+        name={ personsState.persons[1].name } 
+        age={ personsState.persons[1].age }
+        click={ switchNameHandler.bind(this, 'MAX!') }>My hobbies: Racing</Person>
+        {/* recommend using .bind this to improbe performance */}
+      <Person 
+        name={ personsState.persons[2].name }  
+        age={ personsState.persons[2].age } />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
