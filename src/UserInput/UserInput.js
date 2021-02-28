0620a093d40822f1bb47a071cb07ef3a1bff30e1
MMcClure11 <mcclme02@luther.edu>
2021-02-27 18:41:20 -0600

    Add inline styling for UserInput

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+const style = {
+  border: '2px solid purple',
+  padding: '16px',
+  fontSize: '32px',
+  textAlign: 'center',
+}
+
 const UserInput = (props) => {
   return (
     <div>
-      <input type="text" onChange={ props.changed } value={ props.username }/>
+      <input style={ style } type="text" onChange={ props.changed } value={ props.username }/>
     </div>
   )
 }
